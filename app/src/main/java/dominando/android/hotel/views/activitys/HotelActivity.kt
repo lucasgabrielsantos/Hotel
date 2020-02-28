56096daa8b48083996ed955c169fee83fb52a078
lucasgabrielsantos <lucas.gsantos@live.com>
2020-02-28 08:04:40 -0300

    criação do app no modo tablet

--- app/src/main/java/dominando/android/hotel/views/activitys/HotelActivity.kt
+++ app/src/main/java/dominando/android/hotel/views/activitys/HotelActivity.kt
@@ -1,12 +1,15 @@
 package dominando.android.hotel.views.activitys
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import dominando.android.hotel.R
 import dominando.android.hotel.model.Hotel
+import dominando.android.hotel.views.fragments.HotelDetailsFragment
 import dominando.android.hotel.views.fragments.HotelListFragment
 
-class HotelActivity : AppCompatActivity(), HotelListFragment.OnHotelClickListener {
+class HotelActivity : AppCompatActivity(),
+    HotelListFragment.OnHotelClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,10 +17,28 @@ class HotelActivity : AppCompatActivity(), HotelListFragment.OnHotelClickListene
     }
 
     override fun onHotelClick(hotel: Hotel) {
-        showDetailsActivity(hotel.id)
+
+        if (isTablet()) {
+            showDetailsFragment(hotel.id)
+        } else {
+            showDetailsActivity(hotel.id)
+        }
     }
 
     private fun showDetailsActivity(hotelId: Long) {
         HotelDetailsActivity.open(this, hotelId)
     }
+
+    private fun showDetailsFragment(hotelId: Long) {
+        val fragment = HotelDetailsFragment.newInstance(hotelId)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.details, fragment, HotelDetailsFragment.TAG_DETAILS)
+            .commit()
+    }
+
+    private fun isTablet() = resources.getBoolean(R.bool.tablet)
+    private fun isSmartphone() = resources.getBoolean(R.bool.smartphone)
+
+
 }
