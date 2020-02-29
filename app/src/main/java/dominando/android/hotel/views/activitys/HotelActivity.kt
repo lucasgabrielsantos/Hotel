cb6f6deddc99578254d652f5547d4c74c5369d45
lucasgabrielsantos <lucas.gsantos@live.com>
2020-02-28 22:07:07 -0300

    inserindo action bar nas views, finalizando

--- app/src/main/java/dominando/android/hotel/views/activitys/HotelActivity.kt
+++ app/src/main/java/dominando/android/hotel/views/activitys/HotelActivity.kt
@@ -1,15 +1,25 @@
 package dominando.android.hotel.views.activitys
 
 import android.os.Bundle
-import android.view.View
+import android.os.Handler
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import dominando.android.hotel.R
 import dominando.android.hotel.model.Hotel
 import dominando.android.hotel.views.fragments.HotelDetailsFragment
 import dominando.android.hotel.views.fragments.HotelListFragment
 
 class HotelActivity : AppCompatActivity(),
-    HotelListFragment.OnHotelClickListener {
+    HotelListFragment.OnHotelClickListener, SearchView.OnQueryTextListener, MenuItem.OnActionExpandListener {
+
+    private var lastSearchTerm : String = ""
+    private var searchView : SearchView? = null
+
+    private val listFragment : HotelListFragment by lazy {
+        supportFragmentManager.findFragmentById(R.id.fragmentList) as HotelListFragment
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +40,7 @@ class HotelActivity : AppCompatActivity(),
     }
 
     private fun showDetailsFragment(hotelId: Long) {
+        searchView?.setOnQueryTextListener(null)
         val fragment = HotelDetailsFragment.newInstance(hotelId)
         supportFragmentManager
             .beginTransaction()
@@ -37,8 +48,63 @@ class HotelActivity : AppCompatActivity(),
             .commit()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.hotel, menu)
+        val searchItem = menu?.findItem(R.id.action_info)
+        searchItem?.setOnActionExpandListener(this)
+        searchView = searchItem?.actionView as SearchView
+        searchView?.queryHint = getString(R.string.hint_search)
+        searchView?.setOnQueryTextListener(this)
+
+        if (lastSearchTerm.isNotEmpty()){
+            Handler().post {
+                val query = lastSearchTerm
+                searchItem.expandActionView()
+                searchView?.setQuery(query, true)
+                searchView?.clearFocus()
+            }
+        }
+        return true
+    }
+
     private fun isTablet() = resources.getBoolean(R.bool.tablet)
     private fun isSmartphone() = resources.getBoolean(R.bool.smartphone)
 
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        lastSearchTerm = savedInstanceState?.getString(EXTRA_SEARCH_TERM) ?: ""
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState?.putString(EXTRA_SEARCH_TERM, lastSearchTerm)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onQueryTextSubmit(query: String?) = true
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        lastSearchTerm = newText ?: ""
+        listFragment.search(lastSearchTerm)
+        return true
+    }
+
+    override fun onMenuItemActionExpand(item: MenuItem?) = true
+
+    override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
+
+        lastSearchTerm = ""
+        listFragment.clearSearch()
+        return true
+    }
+
+
+    companion object{
+
+        const val EXTRA_SEARCH_TERM = "lastSearch"
+    }
 }
